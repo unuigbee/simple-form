5063977c09f016703821c0bd297f8cf1757d7411
Emmanuel <unuigbee@hotmail.com>
2016-07-04 16:27:40 +0100

    Added CSS modules for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 
 // The plugin configuration sets /client/index.html as a template and creates a brand new index.html
 // file in the /dist folder.
@@ -7,6 +8,10 @@ var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
   template: __dirname + '/client/index.html',
   filename: 'index.html',
   inject: 'body',
+});
+
+var ExtractTextWebpackPluginConfig = new ExtractTextWebpackPlugin('app.css', {
+  allChunks: true,
 });
 
 module.exports = {
@@ -30,8 +35,14 @@ module.exports = {
         // Which includes React(jsx) and es2015 syntax
         loader: 'babel-loader',
       },
+      {
+        test: /\.css$/,
+        // allows us to scope css to particular documents by giving class names unique names
+        // for each css class
+        loader: ExtractTextWebpackPlugin.extract('style-loader', 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'),
+      },
     ],
   },
   // We then add our plugin configuration to the list of Webpack plugins
-  plugins: [HTMLWebpackPluginConfig],
+  plugins: [HTMLWebpackPluginConfig, ExtractTextWebpackPluginConfig],
 };
