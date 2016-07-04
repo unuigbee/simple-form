82f5fb606ec483594c1d157e7bc046ec1355e8fe
Emmanuel <unuigbee@hotmail.com>
2016-07-04 04:01:40 +0100

    Added template components for retrieving server data

---
+++ app/components/Results.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Results = (props) => {
+  return (
+    <div className="jumbotron col-sm-12 text-center">
+      <h1>Results</h1>
+      
+    </div>
+  );
+}
---
+++ app/components/UserDetails.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const UserDetails = (props) => {
+  return (
+    <div>
+      <li>Firstname: </li>
+      <li>Lastname: </li>
+      <li>Email Adress: </li>
+      <li>Telephone: </li>
+      <li>Gender: </li>
+      <li>Comments: </li>
+      <li>Extra Comments: </li>
+    </div>
+  );
+}
